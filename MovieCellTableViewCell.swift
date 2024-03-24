57a7d3413890f01aa83a852c0db1e8f4c454f35e
pragnavi <pragnavi1398@gmail.com>
2024-03-24 17:33:57 -0400

    Created a Custom Table View Cell

---
+++ MovieCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  ios101-lab5-flix1
+//
+//  Created by Pragnavi Ravuluri Sai Durga on 3/24/24.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
